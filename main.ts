98a12988c45a954d1b84c225470615af30070c57
joseantonioarealsousa <130666845+joseantonioarealsousa@users.noreply.github.com>
2023-04-13 13:25:09 +0100

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+let pino12 = 0
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    while (true) {
+    	
+    }
+})
